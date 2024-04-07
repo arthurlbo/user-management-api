b1b5cc823f3fccbbe1b92b325d918e542f9b0696
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2024-04-07 16:52:33 -0300

    refactor: removing study middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,22 +1,14 @@
-import { MiddlewareConsumer, Module, NestModule, RequestMethod } from "@nestjs/common";
+import { Module } from "@nestjs/common";
 
 import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 
 import { UserModule } from "./user/user.module";
-import { IdValidationMiddleware } from "./middlewares/id-validation.middleware";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
-    imports: [UserModule],
+    imports: [AuthModule, UserModule],
     controllers: [AppController],
     providers: [AppService],
 })
-export class AppModule implements NestModule {
-    // The ParseUUIDPipe is a built-in pipe that validates the UUID format already, this is only for study purposes.
-    configure(consumer: MiddlewareConsumer) {
-        consumer.apply(IdValidationMiddleware).forRoutes({
-            path: "*/:id",
-            method: RequestMethod.ALL,
-        });
-    }
-}
+export class AppModule {}
