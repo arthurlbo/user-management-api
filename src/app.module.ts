c664bc88cce5b5c799bc3f57c7df29efff728e72
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2024-04-07 23:16:19 -0300

    feat: adding throttler and config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,7 @@
 import { Module } from "@nestjs/common";
+import { APP_GUARD } from "@nestjs/core";
+import { ConfigModule } from "@nestjs/config";
+import { ThrottlerModule, ThrottlerGuard } from "@nestjs/throttler";
 
 import { AuthModule } from "@/auth/auth.module";
 import { UserModule } from "@/user/user.module";
@@ -7,8 +10,25 @@ import { AppService } from "./app.service";
 import { AppController } from "./app.controller";
 
 @Module({
-    imports: [AuthModule, UserModule],
+    imports: [
+        ThrottlerModule.forRoot([
+            {
+                ttl: 60 * 1000,
+                limit: 10,
+            },
+        ]),
+        ConfigModule.forRoot(),
+        AuthModule,
+        UserModule,
+    ],
     controllers: [AppController],
-    providers: [AppService],
+    providers: [
+        AppService,
+        {
+            provide: APP_GUARD,
+            useClass: ThrottlerGuard,
+        },
+    ],
+    exports: [AppService],
 })
 export class AppModule {}
