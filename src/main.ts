d6c93994b1de1b2c72ce9083b8fa04b80953b803
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2024-04-07 23:16:43 -0300

    feat: enabling cors

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,13 @@ import { ValidationPipe } from "@nestjs/common";
 
 import { AppModule } from "./app.module";
 
-import { LogInterceptor } from "@/interceptors/log.interceptor";
-
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
+    app.enableCors();
     app.useGlobalPipes(new ValidationPipe());
-    app.useGlobalInterceptors(new LogInterceptor());
 
     await app.listen(3000);
 }
+
 bootstrap();
