c59e503be22547e13ca474a6bc5eafeb859cb32a
Arthur Carneiro Lobo <arthurlbo16@gmail.com>
2024-04-15 23:21:12 -0300

    style: running format

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,7 +3,6 @@ import { forwardRef, Module } from "@nestjs/common";
 import { AuthModule } from "@/auth/auth.module";
 import { PrismaModule } from "@/prisma/prisma.module";
 
-
 import { UserService } from "./user.service";
 import { UserController } from "./user.controller";
 
