126051b201b4aeb8f6074d2894ef267dddadc302
arthurlbo <arthurlbo16@gmail.com>
2024-06-20 00:38:57 -0300

    build: upgrading scripts

--- src/test/setup-e2e.ts
+++ src/test/setup-e2e.ts
@@ -3,14 +3,14 @@ import { execSync } from "child_process";
 module.exports = async () => {
     try {
         // Ensure that all the processes associated with testing are fully completed before running the scripts.
-        await new Promise((resolve) => setTimeout(resolve, 3000));
+        await new Promise((resolve) => setTimeout(resolve, 5000));
 
         // Set the database to the test environment and clears it
-        execSync("dotenv -e .env.test -- pnpm clear:db");
+        execSync("pnpm test-p-mg-reset");
         // Set the database to the test environment and run the migrations
-        execSync("dotenv -e .env.test -- pnpm migrate:up");
+        execSync("pnpm test-p-mg");
         // Set the database to the test environment and seed it
-        execSync("dotenv -e .env.test -- pnpm seed");
+        execSync("pnpm test-p-seed");
     } catch (error) {
         console.error("Error during the scripts execution before tests", error);
         process.exit(1);
--- src/test/teardown-e2e.ts
+++ src/test/teardown-e2e.ts
@@ -3,10 +3,10 @@ import { execSync } from "child_process";
 module.exports = async () => {
     try {
         // Ensure that all the processes associated with testing are fully completed before running the scripts.
-        await new Promise((resolve) => setTimeout(resolve, 3000));
+        await new Promise((resolve) => setTimeout(resolve, 5000));
 
         // Set the database to the test environment and clears it after running the tests
-        execSync("dotenv -e .env.test -- pnpm clear:db");
+        execSync("pnpm test-p-mg-reset");
     } catch (error) {
         console.error("Error during the scripts execution after tests", error);
         process.exit(1);
