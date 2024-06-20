e12f2a529ffde396628fe5588e52cb21799f4e93
arthurlbo <arthurlbo16@gmail.com>
2024-06-20 00:38:34 -0300

    chore(eslint): migrating to 9.0.5

---
+++ eslint.config.mjs
@@ -0,0 +1,55 @@
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+import { FlatCompat } from "@eslint/eslintrc";
+import { includeIgnoreFile } from "@eslint/compat";
+
+import js from "@eslint/js";
+import globals from "globals";
+import tsParser from "@typescript-eslint/parser";
+import eslintPlugin from "@typescript-eslint/eslint-plugin";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+});
+
+const gitignorePath = path.resolve(__dirname, ".gitignore");
+
+export default [
+    {},
+    includeIgnoreFile(gitignorePath),
+    ...compat.extends("plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"),
+    {
+        plugins: {
+            "@typescript-eslint": eslintPlugin,
+        },
+
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+
+            parser: tsParser,
+            ecmaVersion: 5,
+            sourceType: "module",
+
+            parserOptions: {
+                project: "tsconfig.json",
+                tsconfigRootDir: "C:\\projects\\user-management-api",
+            },
+        },
+
+        rules: {
+            "@typescript-eslint/interface-name-prefix": "off",
+            "@typescript-eslint/explicit-function-return-type": "off",
+            "@typescript-eslint/explicit-module-boundary-types": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+        },
+    },
+];
