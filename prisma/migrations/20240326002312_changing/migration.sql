25ac8137ea62831fa50943bbee382599712cd66e
arthurlbo <arthurlbo16@gmail.com>
2024-06-20 00:38:47 -0300

    chore: squashing migrations

--- prisma/migrations/20240326002312_changing/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `birthdate` on the `users` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `users` DROP COLUMN `birthdate`,
-    ADD COLUMN `birthDate` DATE NULL;
